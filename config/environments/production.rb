f494dd21dce3a25db9ca9d0121eccd2d7ef59a8e
Øyvind Brungot Dahl <spacetravel.no@gmail.com>
2014-01-13 19:44:01 +0100

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Force SSL
+  config.force_ssl = true
 end
